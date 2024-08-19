1cc81d9cd729280c619c1ccea9566dd2b460d119
dinukah99 <dinukahamarasinghe@gmail.com>
2024-08-19 18:44:41 +0530

    add necessary imports & InvoiceAutomation class with tkinter

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+import datetime as dt
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import docx
+
+
+class InvoiceAutomation:
+
+    def __init__(self):
+        self.root = tk.Tk()
